a4f06c851202730824338463ed3fdbf01bd1c04e
Sarah Leaning <leaning.sarah@gmail.com>
2024-04-09 20:50:18 -0400

    Fix #31

--- src/main/java/degreesmart/project/module-info.java
+++ src/main/java/degreesmart/project/module-info.java
@@ -1,6 +1,7 @@
 module degreesmart.project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens degreesmart.project to javafx.fxml;
     exports degreesmart.project;
